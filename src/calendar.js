fb70035cf28d3da2f7efc384554ea1f88da7c0f1
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-11-05 14:09:32 -0300

    dates array and months array to state

--- src/calendar.js
+++ src/calendar.js
@@ -1,11 +1,26 @@
-import React from "react";
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css'; // optional
-import 'tippy.js/themes/translucent.css';
+import React, { useState, useEffect } from "react";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
+import "tippy.js/themes/translucent.css";
 
 const Calendar = () => {
-  let daysText = ["Mon", "Wed", "Fri"]
-  let monthText = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"]
+  const [datesArr, changeDatesArr] = useState(["Mon", "Wed", "Fri"]);
+  const [monthText, changeMonthText] = useState([
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ]);
+  // const [calendar, changeCalendar] = useState(null);
+
   let days = [];
   for (let i = 1; i <= 366; i++) {
     let d = new Date(2020, 0, i);
@@ -13,86 +28,83 @@ const Calendar = () => {
       date: d,
       title: d.toLocaleDateString("en-US", { month: "long", day: "numeric" }),
       activity: "test",
-      contributions: Math.floor((Math.random() * 100)),
+      contributions: 0, //Math.floor((Math.random() * 100)),
     });
   }
 
   for (let contri of days) {
     if (contri.contributions === 0) {
-        contri.activity = "level_0"
+      contri.activity = "level_0";
     }
-      if (contri.contributions > 0 && contri.contributions <= 5) {
-          contri.activity = "level_1"
-      }
-      if (contri.contributions > 5 && contri.contributions <= 10) {
-        contri.activity = "level_2"
+    if (contri.contributions > 0 && contri.contributions <= 5) {
+      contri.activity = "level_1";
+    }
+    if (contri.contributions > 5 && contri.contributions <= 10) {
+      contri.activity = "level_2";
     }
     if (contri.contributions > 10 && contri.contributions <= 20) {
-        contri.activity = "level_3"
+      contri.activity = "level_3";
     }
     if (contri.contributions > 20 && contri.contributions <= 40) {
-        contri.activity = "level_4"
+      contri.activity = "level_4";
     }
     if (contri.contributions > 40 && contri.contributions <= 70) {
-        contri.activity = "level_5"
+      contri.activity = "level_5";
     }
     if (contri.contributions > 70) {
-        contri.activity = "level_6"
+      contri.activity = "level_6";
     }
   }
 
   const bgColor = (string) => {
-    if (string === "level_0") {
-        return {
-            backgroundColor: "rgb(218, 232, 214)"
-        }
+    switch (string) {
+      case "level_0":
+        return { backgroundColor: "rgb(218, 232, 214)" };
+      case "level_1":
+        return { backgroundColor: "#a4fba6" };
+      case "level_2":
+        return { backgroundColor: "#4ae54a" };
+      case "level_3":
+        return { backgroundColor: "#30cb00" };
+      case "level_4":
+        return { backgroundColor: "#0f9200" };
+      case "level_5":
+        return { backgroundColor: "#006203" };
+      case "level_6":
+        return { backgroundColor: "#041501" };
+      default:
+        return { backgroundColor: "white" };
     }
-    if (string === "level_1") {
-        return {
-            backgroundColor: "#a4fba6"
-        }
-    }
-    if (string === "level_2") {
-        return {
-            backgroundColor: "#4ae54a"
-        }
-    }
-    if (string === "level_3") {
-        return {
-            backgroundColor: "#30cb00"
-        }
-    }
-    if (string === "level_4") {
-        return {
-            backgroundColor: "#0f9200"
-        }
-    }
-    if (string === "level_5") {
-        return {
-            backgroundColor: "#006203"
-        }
-    }
-    if (string === "level_6") {
-        return {
-            backgroundColor: "#041501"
-        }
-    }
-}
+  };
 
   return (
-      <div className="overcontainer">
-      <div className="monthstext">{monthText.map((e,i)=> (<div className="monthtext" key={i}>{e}</div>))}</div>
-      <div className="days">{daysText.map((e,i) => <div className="daystext" key={i}>{e}</div>)}</div>
-    <div class="container">
-      {days.map((e, i) => (
-          <Tippy theme="translucent" content={e.contributions + " contributions on " +e.title} traslucent={true}>
-        <div key={i} className="day" style={bgColor(e.activity)}></div>
-        </Tippy>
-      ))}
+    <div className="overcontainer">
+      <div className="monthstext">
+        {monthText.map((e, i) => (
+          <div className="monthtext" key={i}>
+            {e}
+          </div>
+        ))}
+      </div>
+      <div className="days">
+        {datesArr.map((e, i) => (
+          <div className="daystext" key={i}>
+            {e}
+          </div>
+        ))}
+      </div>
+      <div className="container">
+        {days.map((e, i) => (
+          <Tippy
+            theme="translucent"
+            content={e.contributions + " contributions on " + e.title}
+            key={i}
+          >
+            <div key={i} className="day" style={bgColor(e.activity)}></div>
+          </Tippy>
+        ))}
+      </div>
     </div>
-    </div>
-    
   );
 };
-
 export default Calendar;
